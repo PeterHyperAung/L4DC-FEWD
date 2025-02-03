293c6d3c1e9e28ce0749f65f82014ed248173b1c
Min Phone Aung <88699874+PeterHyperAung@users.noreply.github.com>
2025-02-03 20:13:53 +0630

    slideshow completed

---
+++ js/slideshow.js
@@ -0,0 +1,47 @@
+let currentSlide = 1;
+
+const slideEl = document.getElementById("slideshow-slide");
+const indicatorsList = document.querySelectorAll(".slideshow-indicator");
+
+document
+  .getElementById("slideshow-prev")
+  .addEventListener("click", function () {
+    const prevSlide = currentSlide;
+    currentSlide = currentSlide > 1 ? currentSlide - 1 : 3;
+    slideEl.classList.remove("slideshow--slide-" + prevSlide);
+    slideEl.classList.add("slideshow--slide-" + currentSlide);
+    indicatorsList.forEach((indicator) => {
+      indicator.classList.remove("slideshow--indicator-active");
+    });
+    document
+      .getElementById("indicator-" + currentSlide)
+      .classList.add("slideshow--indicator-active");
+  });
+
+document
+  .getElementById("slideshow-next")
+  .addEventListener("click", function () {
+    const prevSlide = currentSlide;
+    currentSlide = currentSlide < 3 ? currentSlide + 1 : 1;
+    slideEl.classList.remove("slideshow--slide-" + prevSlide);
+    slideEl.classList.add("slideshow--slide-" + currentSlide);
+    indicatorsList.forEach((indicator) => {
+      indicator.classList.remove("slideshow--indicator-active");
+    });
+    document
+      .getElementById("indicator-" + currentSlide)
+      .classList.add("slideshow--indicator-active");
+  });
+
+indicatorsList.forEach((indicator) => {
+  indicator.addEventListener("click", function () {
+    const prevSlide = currentSlide;
+    currentSlide = parseInt(indicator.id.split("-")[1]);
+    slideEl.classList.remove("slideshow--slide-" + prevSlide);
+    slideEl.classList.add("slideshow--slide-" + currentSlide);
+    indicatorsList.forEach((indicator) => {
+      indicator.classList.remove("slideshow--indicator-active");
+    });
+    indicator.classList.add("slideshow--indicator-active");
+  });
+});
